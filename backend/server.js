1afe0e10bf0a4571fe78635168c5fbc94a50ec62
ramde solanki <ramdesolanki2511@gmail.com>
2026-06-22 21:49:08 +0530

    User registration and login API completed

--- backend/server.js
+++ backend/server.js
@@ -1,24 +1,27 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-require('dotenv').config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
+const authRoutes = require("./routes/authRoutes");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-mongoose
-.connect(process.env.MONGO_URI)
-.then(() => console.log('MongoDB Connected'))
-.catch(err => console.log(err));
+app.use("/api/auth", authRoutes);
 
-app.get('/', (req,res)=>{
-    res.send('API Running');
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.log(err));
+
+app.get("/", (req, res) => {
+  res.send("API Running");
 });
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT,()=>{
-    console.log(`Server Running ${PORT}`);
+app.listen(PORT, () => {
+  console.log(`Server Running ${PORT}`);
 });
